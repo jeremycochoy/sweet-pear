9a26370e2020e3f87872d8a45faf6bc66ff3a994
Jérémy Cochoy <jeremycochoy@MacBook-Pro-de-Jeremy.local>
2019-07-25 16:54:55 +0200

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sweet-pear",
-    version="0.0.3",
+    version="0.0.4",
     author="Jeremy Cochoy",
     author_email="jeremy.cochoy@gmail.com",
     description="Add basic functional methods to standard types",
