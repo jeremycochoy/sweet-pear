f430edc5adcf472950992faaf840fc57f5826256
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2019-08-06 11:43:32 +0200

    Handle both methods and properties

--- README.md
+++ README.md
@@ -4,8 +4,25 @@ Sweet pear is a python package wich add
 basic functional methods to the core types
 of python.
 
+This is really usefull for fast prototyping / debugging.
+
 Usage example:
 ```
 (1, 2, 3, 4).map(lambda x: x**2)
 ["hello", "world"].map('capitalize')
 ```
+
+You can also is this property to access member of objects:
+```
+import numpy as np
+import torch
+
+infos = (np.array([1,2,3]), np.zeros((2,5)), torch.ones((3,4)))
+infos.map('shape')
+```
+
+# How does it works
+
+It rely on ForbiddenFruit to patch the built in types in order to add the `map` method.
+The implementation is pretty straight forward and simply rely on python's map implementation.
+
--- sweetpear/__init__.py
+++ sweetpear/__init__.py
@@ -4,15 +4,31 @@ name = "sweetpear"
 from forbiddenfruit import curse
 
 
+def __work_with_string(x, member):
+    """
+    Return the result of x.#member or x.#member()
+    where member is the name of the function/variable.
+
+    :param x: Element to operate on
+    :param member: Function or value member
+    :return:
+    """
+    action = getattr(x, member)
+    if callable(action):
+        return action()
+    else:
+        return action
+
+
 def __curse_list_map(self, fct):
     if type(fct) == str:
-        return list(map(lambda x: getattr(x, fct)(), self))
+        return list(map(lambda x: __work_with_string(x, fct), self))
     return list(map(fct, self))
 
 
 def __curse_tuple_map(self, fct):
     if type(fct) == str:
-        return tuple(map(lambda x: getattr(x, fct)(), self))
+        return tuple(map(lambda x: __work_with_string(x, fct), self))
     return tuple(map(fct, self))
 
 
