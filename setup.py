1b76b303fbebbd9e97a66771eb5ccf8ca7b65105
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2020-01-04 15:13:55 +0200

    Release version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sweet-pear",
-    version="0.2.0",
+    version="1.0.0",
     author="Jeremy Cochoy",
     author_email="jeremy.cochoy@gmail.com",
     description="Add basic functional methods to standard types",
